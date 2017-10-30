3baea863736303cc59ed0a1677ef80655133b4fb
Piotr Małecki-Jurek <piotr.m.jurek@gmail.com>
2017-10-30 18:29:15 +0100

    Hello world

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Habit Tracker!</h1>
+</template>
+
+<script>
+  export default {
+    // tu kod komponentu
+  }
+
+</script>
+
+<style>
+  html, body {
+    margin: 0;
+    padding: 0;
+    background: #34495E;
+  }
+  h1 {
+    color: #41B883;
+  }
+</style>
---
+++ main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Element from 'element-ui'
+import App from './App'
+
+Vue.use(Element)
+
+new Vue({
+  el: '#app',
+  render: (h) => h(App)
+}) 
