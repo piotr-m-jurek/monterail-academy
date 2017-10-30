a07746dc1b1acaf78fb35adc04e44f19bf6b2135
Piotr Małecki-Jurek <piotr.m.jurek@gmail.com>
2017-10-30 18:53:01 +0100

    podstawowa lista

--- App.vue
+++ App.vue
@@ -1,10 +1,29 @@
 <template>
-  <h1>Habit Tracker!</h1>
+  <el-containter>
+    <el-header>
+      <h1>Header</h1>
+    </el-header>
+    <el-main>
+      <div :gutter="8" v-for="(habit, key) in habits" :key="key" class="habit-card">
+        {{habit}}
+      </div>
+    </el-main>
+  </el-containter>
 </template>
 
 <script>
   export default {
-    // tu kod komponentu
+    data () {
+      return {
+        habits: [
+          '1',
+          '2',
+          '3',
+          '4',
+          '5',
+        ]
+      }
+    }
   }
 
 </script>
@@ -13,9 +32,18 @@
   html, body {
     margin: 0;
     padding: 0;
-    background: #34495E;
+    background: #abc;
   }
-  h1 {
-    color: #41B883;
+
+  .habit-card {
+    background: white;
+    padding: 16px;
+    border-radius: 4px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
   }
+
+  .habit-card + .habit-card {
+    margin-top: 4px;
+  }
+
 </style>
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Element from 'element-ui'
 import App from './App'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Element)
 
